1e9b73aeb070faf77f5397e34881e606015e5b78
Bear <99shiya@gmail.com>
2021-06-06 18:57:17 +0800

    加入專案檔案。

---
+++ BS_Adoga/Controllers/HotelDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BS_Adoga.Controllers
+{
+    public class HotelDetailController : Controller
+    {
+        // GET: HotelDetail
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ BS_Adoga/Controllers/MemberLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BS_Adoga.Controllers
+{
+    public class MemberLoginController : Controller
+    {
+        // GET: MemberLogin
+        public ActionResult MemberLogin()
+        {
+            return View();
+        }
+    }
+}
