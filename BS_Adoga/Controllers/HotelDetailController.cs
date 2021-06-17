dd8b7c8ceff77f05764520bc54e8c49a74a79737
Bear <99shiya@gmail.com>
2021-06-18 03:20:03 +0800

    成功測試Repository和Service的做法，更改了我的ViewModel的名稱

--- BS_Adoga/Controllers/HotelDetailController.cs
+++ BS_Adoga/Controllers/HotelDetailController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using BS_Adoga.Models.DBContext;
 using BS_Adoga.Models.ViewModels.HotelDetail;
+using BS_Adoga.Service.HotelDetail;
 
 namespace BS_Adoga.Controllers
 {
@@ -17,15 +18,21 @@ namespace BS_Adoga.Controllers
         }
 
         // GET: HotelDetail
-        public ActionResult Detail()
+        public ActionResult Detail(string hotelName)
         {
-            var hotel = from h in _context.Hotels
-                        join r in _context.Rooms
-                        on h.HotelID equals r.HotelID
-                        where h.HotelName == "台中商旅 (Hung's Mansion)"
-                        select new HotelDetail { HotelID = h.HotelID,HotelName = h.HotelName,HotelAddress=h.HotelAddress,Star=h.Star };
+            //var hotel = from h in _context.Hotels
+            //            join r in _context.Rooms
+            //            on h.HotelID equals r.HotelID
+            //            where h.HotelName == "台中商旅 (Hung's Mansion)"
+            //            select new HotelViewModel { HotelID = h.HotelID,HotelName = h.HotelName,HotelAddress=h.HotelAddress,Star=h.Star };
 
-            return View(hotel.FirstOrDefault());
+            //return View(hotel.FirstOrDefault());
+
+            HotelDetailService service = new HotelDetailService();
+
+            var hotel = service.GetHotel(hotelName);
+
+            return View(hotel);
         }
 
         //public ActionResult DetailAlbum()
--- BS_Adoga/Models/ViewModels/HotelDetail/HotelViewModel.cs
+++ BS_Adoga/Models/ViewModels/HotelDetail/HotelViewModel.cs
@@ -5,19 +5,23 @@ using System.Web;
 
 namespace BS_Adoga.Models.ViewModels.HotelDetail
 {
-    public class HotelDetail
+    public class HotelViewModel
     {
         public string HotelID { get; set; }
 
         public string HotelName { get; set; }
 
+        public string HotelEngName { get; set; }
+
+        public string HotelCity { get; set; }
+
         public string HotelAddress { get; set; }
 
-        //public string HotelAbout { get; set; }
+        public string HotelAbout { get; set; }
 
-        //public decimal? Longitude { get; set; }
+        public decimal? Longitude { get; set; }
 
-        //public decimal? Latitude { get; set; }
+        public decimal? Latitude { get; set; }
 
         public int Star { get; set; }
 
