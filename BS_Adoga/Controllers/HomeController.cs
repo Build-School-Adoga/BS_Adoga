eb12c3889792673a6107aa11fe641796ae0ca306
劉政謙 <b10606015@ms.chu.edu.tw>
2021-06-18 21:00:40 +0800

    圖片變成內建

--- BS_Adoga/Controllers/HomeController.cs
+++ BS_Adoga/Controllers/HomeController.cs
@@ -44,10 +44,10 @@ namespace BS_Adoga.Controllers
         {
             List<Card> cards = new List<Card>
             {
-                new Card { name = "台北W酒店", area = "信義區", Evaluation = 2, picture = "https://pix6.agoda.net/hotelImages/335/335043/335043_16121920250049783861.jpg", ground = "https://pix6.agoda.net/hotelImages/335/335043/335043_16121920250049783861.jpg", sale = "2.5折扣", Originalprice = "NT$11,024", saleprice = "NT$5,500", fraction = "9.3", comment = "超棒", Quantity = "1198篇評鑑" },
-                 new Card { name = "飛行家青年旅館", area= "苓雅區", Evaluation=  1, picture= "https://pix6.agoda.net/hotelImages/5059720/0/feb524ebbcc31042755392d15d8cef3b.jpg", ground= "https://pix6.agoda.net/hotelImages/5059720/0/feb524ebbcc31042755392d15d8cef3b.jpg", sale= "2.2折扣", Originalprice= "NT$2,370", saleprice= "NT$595", fraction= "8.8", comment= "很讚", Quantity= "1198篇評鑑" },
-                  new Card  { name= "鈞怡大飯店", area= "高雄市", Evaluation=  3, picture= "https://pix6.agoda.net/hotelImages/335/335043/335043_16121920250049783861.jpg", ground= "https://pix6.agoda.net/hotelImages/4612444/-1/a9f27a564ebc9951ddc5cfc12970dfaa.jpg?s=450x450", sale= "1.9折扣", Originalprice= "NT$6,234", saleprice= "NT$1,234", fraction= "8.9", comment= "很讚", Quantity= "1198篇評鑑" },
-                  new Card  { name= "塩‧泊思行旅", area= "高雄市", Evaluation= 4, picture= "https://pix6.agoda.net/hotelImages/21874089/-1/8a6235eaf9b5c0ae0a9484b253996530.jpg?s=450x450", ground= "https://pix6.agoda.net/hotelImages/21874089/-1/8a6235eaf9b5c0ae0a9484b253996530.jpg?s=450x450", sale= "2折扣", Originalprice= "NT$3,611", saleprice= "NT$764", fraction= "8.4", comment= "很讚", Quantity= "1198篇評鑑"}
+                new Card { name = "台北W酒店", area = "信義區", Evaluation = 2,  ground ="../Asset/images/Home/台北w酒店.jpg", sale = "2.5折扣", Originalprice = "NT$11,024", saleprice = "NT$5,500", fraction = "9.3", comment = "超棒", Quantity = "1198篇評鑑" },
+                 new Card { name = "飛行家青年旅館", area= "苓雅區", Evaluation=  1, ground= "../Asset/images/Home/飛行家青年旅館.jpg", sale= "2.2折扣", Originalprice= "NT$2,370", saleprice= "NT$595", fraction= "8.8", comment= "很讚", Quantity= "1198篇評鑑" },
+                  new Card  { name= "鈞怡大飯店", area= "高雄市", Evaluation=  3,  ground= "../Asset/images/Home/鈞怡大飯店.jpg", sale= "1.9折扣", Originalprice= "NT$6,234", saleprice= "NT$1,234", fraction= "8.9", comment= "很讚", Quantity= "1198篇評鑑" },
+                  new Card  { name= "塩‧泊思行旅", area= "高雄市", Evaluation= 4,  ground= "../Asset/images/Home/塩‧泊思行旅.jpg", sale= "2折扣", Originalprice= "NT$3,611", saleprice= "NT$764", fraction= "8.4", comment= "很讚", Quantity= "1198篇評鑑"}
 
                  };
             //return View(cards);
