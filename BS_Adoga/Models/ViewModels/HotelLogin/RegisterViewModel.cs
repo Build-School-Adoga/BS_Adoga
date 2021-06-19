75a7dc4d9bfb89a7f0d32209a8c85cb020a59abd
vm6vul3vup <vm6vul3vup@gmail.com>
2021-06-19 19:58:24 +0800

    修改RegisterViewModel密碼複雜度驗證

--- BS_Adoga/Models/ViewModels/HotelLogin/RegisterViewModel.cs
+++ BS_Adoga/Models/ViewModels/HotelLogin/RegisterViewModel.cs
@@ -25,13 +25,18 @@ namespace BS_Adoga.Models.ViewModels.HotelLogin
         [Display(Name = "密碼")]
         [DataType(DataType.Password)]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "密碼的長度需再6~20個字元內！")]
+        //(?!.*[^\x21-\x7e])不允許特殊符號或數字英文字母以外的東西輸入
+        //(?=.*[\W])至少有一個特殊符號
+        //(?=.*[a-zA-Z])大小寫至少各一
+        //(?=.*\d)0-9的數字各一
+        [RegularExpression(@"^(?!.*[^\x21-\x7e])(?=.*[\W])(?=.*[a-zA-Z])(?=.*\d).*$", ErrorMessage = "密碼需要包含最少一位的大小寫英文字母與數字與一個以上的特殊符號")]
         public string Password { get; set; }
 
         [NotMapped]
         [Required]
         [Display(Name = "確認密碼")]
         [DataType(DataType.Password)]
-        [StringLength(20, MinimumLength = 6, ErrorMessage = "密碼的長度需再6~20個字元內！")]
+        [StringLength(20)]
         [Compare("Password", ErrorMessage = "兩次輸入的密碼必須相符！")]
         public string ConfirmPassword { get; set; }
 
