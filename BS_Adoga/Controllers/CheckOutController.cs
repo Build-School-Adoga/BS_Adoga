6fd0ccff489172b8d8cd8e3b8d61f60cb0620a80
Eliot <eliot.lin.0930@gmail.com>
2021-06-20 20:40:27 +0800

    Up

--- BS_Adoga/Controllers/CheckOutController.cs
+++ BS_Adoga/Controllers/CheckOutController.cs
@@ -1,15 +1,30 @@
-﻿using System;
+﻿using BS_Adoga.Models.DBContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BS_Adoga.Models.ViewModels.CheckOut;
 
 namespace BS_Adoga.Controllers
 {
     public class CheckOutController : Controller
     {
+        private AdogaContext _context;
+        public CheckOutController()
+        {
+            _context = new AdogaContext();
+        }
+
         // GET: CheckOut
         public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CheckOutListViewModel model)
         {
             return View();
         }
---
+++ BS_Adoga/Models/ViewModels/CheckOut/CheckOutListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BS_Adoga.Models.ViewModels.CheckOut
+{
+    public class CheckOutListViewModel
+    {
+        //public string OrderID { get; set; }
+
+        [DisplayName("英文名（同護照）")]
+        public string FirstName { get; set; }
+
+        [DisplayName("英文姓（同護照）")]
+        public string LastName { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("手機號碼")]
+        public string PhoneNumber { get; set; }
+
+        //public SelectList Country { get; set; }
+
+    }
+}
