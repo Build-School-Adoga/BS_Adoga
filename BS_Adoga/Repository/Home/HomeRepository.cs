3e68ecb28e22108a20dcc8f33d6243d05ca1c1c7
Bear <99shiya@gmail.com>
2021-06-24 21:19:16 +0800

    嘗試Debug

--- BS_Adoga/Repository/Home/HomeRepository.cs
+++ BS_Adoga/Repository/Home/HomeRepository.cs
@@ -15,13 +15,13 @@ namespace BS_Adoga.Repository.Home
             _context = new AdogaContext();
         }
         List<Card> cards = new List<Card>
-            {
-                new Card { name = "台北W酒店", area = "信義區", Evaluation = 2,  ground ="../Asset/images/Home/台北w酒店.jpg", sale = "2.5折扣", Originalprice = "NT$11,024", saleprice = "NT$5,500", fraction = "9.3", comment = "超棒", Quantity = "1198篇評鑑" },
-                 new Card { name = "飛行家青年旅館", area= "苓雅區", Evaluation=  1, ground= "../Asset/images/Home/飛行家青年旅館.jpg", sale= "2.2折扣", Originalprice= "NT$2,370", saleprice= "NT$595", fraction= "8.8", comment= "很讚", Quantity= "1198篇評鑑" },
-                  new Card  { name= "鈞怡大飯店", area= "高雄市", Evaluation=  3,  ground= "../Asset/images/Home/鈞怡大飯店.jpg", sale= "1.9折扣", Originalprice= "NT$6,234", saleprice= "NT$1,234", fraction= "8.9", comment= "很讚", Quantity= "1198篇評鑑" },
-                  new Card  { name= "塩‧泊思行旅", area= "高雄市", Evaluation= 4,  ground= "../Asset/images/Home/塩‧泊思行旅.jpg", sale= "2折扣", Originalprice= "NT$3,611", saleprice= "NT$764", fraction= "8.4", comment= "很讚", Quantity= "1198篇評鑑"}
+        {
+            new Card { name = "台北W酒店", area = "信義區", Evaluation = 2,  ground ="../Asset/images/Home/台北w酒店.jpg", sale = "2.5折扣", Originalprice = "NT$11,024", saleprice = "NT$5,500", fraction = "9.3", comment = "超棒", Quantity = "1198篇評鑑" },
+            new Card { name = "飛行家青年旅館", area= "苓雅區", Evaluation=  1, ground= "../Asset/images/Home/飛行家青年旅館.jpg", sale= "2.2折扣", Originalprice= "NT$2,370", saleprice= "NT$595", fraction= "8.8", comment= "很讚", Quantity= "1198篇評鑑" },
+            new Card  { name= "鈞怡大飯店", area= "高雄市", Evaluation=  3,  ground= "../Asset/images/Home/鈞怡大飯店.jpg", sale= "1.9折扣", Originalprice= "NT$6,234", saleprice= "NT$1,234", fraction= "8.9", comment= "很讚", Quantity= "1198篇評鑑" },
+            new Card  { name= "塩‧泊思行旅", area= "高雄市", Evaluation= 4,  ground= "../Asset/images/Home/塩‧泊思行旅.jpg", sale= "2折扣", Originalprice= "NT$3,611", saleprice= "NT$764", fraction= "8.4", comment= "很讚", Quantity= "1198篇評鑑"}
 
-                 };
+        };
 
 
 
@@ -31,7 +31,8 @@ namespace BS_Adoga.Repository.Home
             {
                 Cards = cards,
                 Hotels = _context.Hotels.ToList()
-            };
+            };            
+
             return productss;
         }
 
