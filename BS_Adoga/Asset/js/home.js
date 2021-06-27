aca964a207a287d6f184ccd399f031025775cf8d
劉政謙 <b10606015@ms.chu.edu.tw>
2021-06-27 16:51:38 +0800

    垃圾

---
+++ BS_Adoga/Asset/js/home.js
@@ -0,0 +1,14 @@
+﻿
+function dropright() {
+    var droprightBox = document.getElementsByClassName("dropdown-menu");
+    // debugger;
+    // droprightBox.style.visibility = "visible";
+
+    //droprightBox.addEventListener('click', function () {
+
+    //})
+    //下拉框查询组件点击查询栏时不关闭下拉框
+    //需要在tag裡加入data-stopPropagation="true"才能執行成功
+    event.stopPropagation();
+
+}
