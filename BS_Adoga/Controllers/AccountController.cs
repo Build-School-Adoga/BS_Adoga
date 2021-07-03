4c7375bfed90e0fb0120d3a02f5a06524da0b9f4
linjason1688 <lin.jason1688@gmail.com>
2021-07-03 21:19:31 +0800

    前端頁面抓取資料庫內容

--- BS_Adoga/Controllers/AccountController.cs
+++ BS_Adoga/Controllers/AccountController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using BS_Adoga.Service.Account;
 using System.Web.Security;
-using BS_Adoga.Service.Account;
+using BS_Adoga.Models.DBContext;
 
 namespace BS_Adoga.Controllers
 {
@@ -32,9 +32,23 @@ namespace BS_Adoga.Controllers
             ViewBag.MemberCurrentPage = "profile";
 
 
-            string id = ((FormsIdentity)HttpContext.User.Identity).Ticket.UserData;
 
-            return View();
+            AdogaContext db = new AdogaContext();
+            string user_id = ((FormsIdentity)HttpContext.User.Identity).Ticket.UserData;
+            //var data = from m in db.Customers
+            //           where m.CustomerID == user_id
+            //           select m;
+
+            var data = db.Customers.Where(x => x.CustomerID == user_id).FirstOrDefault();
+            return View(data);
+
+            
+
+            //使用表單驗證來進行使用者身份識別，當Http的連線字串等於使用者的真確性，回傳表單驗證的使用者特定資料(使用者特定字串)
+            //字串 id = ((使用表單驗證來進行使用者的身份識別) Http連接字串.使用者.身份識別).取得表單驗證的使用者身份識別.取得存放使用者的特定字串
+            //string id = ((FormsIdentity)HttpContext.User.Identity).Ticket.UserData;
+
+            //return View();
         }
     }
 }
---
+++ BS_Adoga/Models/ViewModels/Account/MemberAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using BS_Adoga.Models.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BS_Adoga.Models.ViewModels.Account
+{
+    public class MemberAccountRepository
+    {
+        //private AdogaContext _context;
+    }
+}
